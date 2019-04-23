13cc6fcca008fdc848fdee628a54ab1032361971
Mitchell Black <mitchellblack@Mitchells-MacBook-Pro.local>
2019-04-23 14:49:49 -0600

    I think they probably use snake_case for python because of the good word play

--- Extractor.py
+++ Extractor.py
@@ -5,7 +5,7 @@ import os
 import random
 
 
-def extract_features(image_path, alg, vector_size=32):
+def extract_features(image_path, alg):
     image = cv2.imread(image_path)
 
     try:
@@ -32,28 +32,28 @@ def batch_extractor(images_path, alg, pickled_db_path ):
 
 def main():
     # paths to the thumbnails and the test images
-    thumbnailPath='./Pictures'
-    testPath = './TestImages'
+    thumbnail_path='./Pictures'
+    test_path = './TestImages'
 
     # path to write pickled feature vectors. naming convention: {pictures, test}FV.pck
     picklePath = 'picturesKaze.pck'
 
-    # all the feature vectors. don't take too long to build so I build them all instead of switching 'em out
+    # all the keypoint descriptors
     kaze = cv2.KAZE_create()
     orb = cv2.ORB_create()
     akaze = cv2.AKAZE_create()
     brisk = cv2.BRISK_create()
 
-    batch_extractor(images_path=thumbnailPath, alg=kaze, pickled_db_path='picturesKAZE.pck')
-    batch_extractor(images_path=testPath, alg=kaze, pickled_db_path='testKAZE.pck')
+    batch_extractor(images_path=thumbnail_path, alg=kaze, pickled_db_path='picturesKAZE.pck')
+    batch_extractor(images_path=test_path, alg=kaze, pickled_db_path='testKAZE.pck')
 
-    batch_extractor(images_path=thumbnailPath, alg=akaze, pickled_db_path='picturesAKAZE.pck')
-    batch_extractor(images_path=testPath, alg=akaze, pickled_db_path='testAKAZE.pck')
+    batch_extractor(images_path=thumbnail_path, alg=akaze, pickled_db_path='picturesAKAZE.pck')
+    batch_extractor(images_path=test_path, alg=akaze, pickled_db_path='testAKAZE.pck')
 
-    batch_extractor(images_path=thumbnailPath, alg=orb, pickled_db_path='picturesORB.pck')
-    batch_extractor(images_path=testPath, alg=orb, pickled_db_path='testORB.pck')
+    batch_extractor(images_path=thumbnail_path, alg=orb, pickled_db_path='picturesORB.pck')
+    batch_extractor(images_path=test_path, alg=orb, pickled_db_path='testORB.pck')
 
-    batch_extractor(images_path=thumbnailPath, alg=brisk, pickled_db_path='picturesBRISK.pck')
-    batch_extractor(images_path=testPath, alg=brisk, pickled_db_path='testBRISK.pck')
+    batch_extractor(images_path=thumbnail_path, alg=brisk, pickled_db_path='picturesBRISK.pck')
+    batch_extractor(images_path=test_path, alg=brisk, pickled_db_path='testBRISK.pck')
 
 main()
--- FeatureVectorMatcher.py
+++ FeatureVectorMatcher.py
@@ -8,107 +8,67 @@ import scipy.spatial
 import random
 from timeit import default_timer as timer
 
-def singleCompare(testImg, pickled_db_path='picturesKAZE.pck'):
-    print("unpickling")
-    with open(pickled_db_path, 'rb') as fp:
-        data = pickle.load(fp)
-        thumbNames = []
-        thumbMatrix = []
-        for k, v in data.items():
-            thumbNames.append(k)
-            thumbMatrix.append(v)
+'''
+    Search for all the images in test_pck_path in pictures_pck_path
 
-        thumbMatrix = np.array(matrix)
-        thumbNames = np.array(names)
-    print("donepickling")
-
-    mostKPsInCommon = -1
-    bestPicture = None
-
-
-    img1 = cv.imread(testImg,0)
-    print('creating kaze')
-    kaze = cv.KAZE_create()
-    print('computing features')
-    kp1, des1 = kaze.detectAndCompute(img1,None)
-    bf = cv.BFMatcher()
-
-    i = 0
-    for des in matrix:
-
-        matches = bf.knnMatch(des1,des, k=2)
-
-        good = []
-        for m,n in matches:
-            if m.distance < 0.75*n.distance:
-                good.append([m])
-
-        print(names[i] + ': ' + str(len(good)))
-        if len(good) > mostKPsInCommon:
-            mostKPsInCommon = len(good)
-            bestPicture = names[i]
-            # bestIMG = img2
-            # bestKPs = kp2
-            # bestGood = good
-
-        i = i + 1
-
-    print("Winner: " + bestPicture)
-
-
-def batchCompare(test_pck_path, pictures_pck_path, file, hamming=False):
+    file is the output file
+    hamming is a boolean for hamming or euclidean distance
+'''
+def batch_compare(test_pck_path, pictures_pck_path, file, hamming=False):
     f = open(file, 'w')
 
     print("unpickling test")
     with open(test_pck_path, 'rb') as fp:
         data = pickle.load(fp)
-        testNames = []
-        testMatrix = []
+        test_names = []
+        test_matrix = []
         for k, v in data.items():
-            testNames.append(k)
-            testMatrix.append(v)
-        testMatrix = np.array(testMatrix)
-        testNames = np.array(testNames)
+            test_names.append(k)
+            test_matrix.append(v)
+        test_matrix = np.array(test_matrix)
+        test_names = np.array(test_names)
     print("donepickling test")
 
 
     print("unpickling thumbnails")
     with open(pictures_pck_path, 'rb') as fp:
         data = pickle.load(fp)
-        thumbNames = []
-        thumbMatrix = []
+        thumb_names = []
+        thumb_matrix = []
         for k, v in data.items():
-            thumbNames.append(k)
-            thumbMatrix.append(v)
+            thumb_names.append(k)
+            thumb_matrix.append(v)
 
-        thumbMatrix = np.array(thumbMatrix)
-        thumbNames = np.array(thumbNames)
+        thumb_matrix = np.array(thumb_matrix)
+        thumb_names = np.array(thumb_names)
     print("donepickling thumbnails")
 
-    j = 0
+
     if hamming:
         FLANN_INDEX_LSH = 6
         index_params= dict(algorithm = FLANN_INDEX_LSH,
-                       table_number = 6, # 12
-                       key_size = 12,     # 20
-                       multi_probe_level = 1) #2
+                       table_number = 6,
+                       key_size = 12,
+                       multi_probe_level = 1)
     else:
-        # create flann mathcer
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 
-    search_params = dict(checks=50)   # or pass empty dictionary
+    search_params = dict(checks=50)
+    # flann implements approximate nearest neighbor search
     flann = cv.FlannBasedMatcher(index_params,search_params)
-    for testDes in testMatrix:
-        mostKPsInCommon = -1
-        bestPicture = None
+
+    j = 0
+    for test_des in test_matrix:
+        most_kps_in_common = -1
+        best_picture = None
 
         i = 0
-        for thumbDes in thumbMatrix:
+        for thumb_des in thumb_matrix:
             good = []
-            if not thumbDes is None and not testDes is None:
+            if not thumb_des is None and not test_des is None:
 
-                matches = flann.knnMatch(testDes,thumbDes,k=2)
+                matches = flann.knnMatch(test_des,thumb_des,k=2)
 
                 for match in matches:
                     if len(match) > 1:
@@ -117,45 +77,45 @@ def batchCompare(test_pck_path, pictures_pck_path, file, hamming=False):
                             good.append([m])
 
 
-            if len(good) > mostKPsInCommon:
-                mostKPsInCommon = len(good)
-                bestPicture = thumbNames[i]
+            if len(good) > most_kps_in_common:
+                most_kps_in_common = len(good)
+                best_picture = thumb_names[i]
 
             i = i + 1
 
 
-
-        if testNames[j] == bestPicture:
+        # k=1 means 1 success. this also easy counting of successful runs
+        if test_names[j] == best_picture:
             k=1
         else:
             k=0
-        # print(testNames[j] +  "," + bestPicture + ", " + str(k))
-        f.write(testNames[j] +  "," + bestPicture + ", " + str(k))
+        # print(test_names[j] +  "," + best_picture + ", " + str(k))
+        f.write(test_names[j] +  "," + best_picture + ", " + str(k))
 
         j = j+1
     f.close()
 
 if __name__=='__main__':
     start_time = timer()
-    batchCompare('testKAZE.pck', 'picturesKAZE.pck', 'KAZEoutput.csv',hamming=False)
+    batch_compare('testKAZE.pck', 'picturesKAZE.pck', 'KAZEoutput.csv',hamming=False)
     end_time = timer()
     time = end_time - start_time
     print("time to run kaze: " + str(time))
 
     start_time = timer()
-    batchCompare('testORB.pck', 'picturesORB.pck', 'ORBoutput.csv',hamming=True)
+    batch_compare('testORB.pck', 'picturesORB.pck', 'ORBoutput.csv',hamming=True)
     end_time = timer()
     time = end_time - start_time
     print("time to run orb: " + str(time))
 
     start_time = timer()
-    batchCompare('testAKAZE.pck', 'picturesAKAZE.pck', 'AKAZEoutput.csv', hamming=True)
+    batch_compare('testAKAZE.pck', 'picturesAKAZE.pck', 'AKAZEoutput.csv', hamming=True)
     end_time = timer()
     time = end_time - start_time
     print("time to run akaze: " + str(time))
 
     start_time = timer()
-    batchCompare('testBRISK.pck', 'picturesBRISK.pck', 'BRISKoutput.csv', hamming=True)
+    batch_compare('testBRISK.pck', 'picturesBRISK.pck', 'BRISKoutput.csv', hamming=True)
     end_time = timer()
     time = end_time - start_time
     print("time to run brisk: " + str(time))
